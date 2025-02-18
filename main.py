a5b8a2856b6597e785779adca5961f436a3ae2ce
Nikolay Butnik <btnk.nik@gmail.com>
2025-02-18 17:22:03 -0500

    Implementation of auto scroll

--- main.py
+++ main.py
@@ -19,7 +19,44 @@ from schemas import RawCompanyModel
 load_dotenv()
 
 
+# TODO: need to find a way to wait for scrolling to finish before generating markdown
 async def main():
+    session_id = "maps_results"
+    base_wait = """js:() => {
+        const resultItems = document.querySelectorAll('div[role='feed'] > div:not([class])');
+        return resultItems.length > 0;
+    }"""
+
+    js_scroll = """
+        // timeout of 5 seconds per scroll.
+        async function autoScroll(containerSelector, delay = 5000) {
+            const container = document.querySelector(containerSelector);
+            if (!container) {
+                console.error("Container not found:", containerSelector);
+                return;
+            }
+
+            let previousHeight = 0;
+
+            while (true) {
+                const resultItems = document.querySelectorAll("div[role='feed'] > div:not([class])");
+                console.log("Result count: " + resultItems.length);
+                container.scrollTo(0, container.scrollHeight);
+                await new Promise(resolve => setTimeout(resolve, delay));
+
+                const newHeight = container.scrollHeight;
+                if (newHeight === previousHeight) {
+                    console.log("No more content is loading.");
+                    break;
+                }
+
+                previousHeight = newHeight;
+            }
+        }
+
+        autoScroll("div[role='feed']");
+    """
+
     proxy_config = {
         "server": os.getenv("PROXY_URL"),
         "username": os.getenv("PROXY_USERNAME"),
@@ -36,14 +73,16 @@ async def main():
         input_format="markdown",
         instruction="For every valid result entry extract company name, address (including street, city, province/state, and country), and website url",
     )
-    crawler_config = CrawlerRunConfig(
+    initial_crawler_config = CrawlerRunConfig(
         cache_mode=CacheMode.BYPASS,
+        session_id=session_id,
         extraction_strategy=llm_strategy,
         only_text=True,
         remove_overlay_elements=True,
         exclude_external_images=True,
-        wait_for="div[role='feed']",
-        css_selector="div[role='feed']",
+        wait_for=base_wait,
+        js_code=js_scroll,
+        # css_selector="div[role='feed']",
     )
 
     lang = "hl=en&gl=CA"
@@ -52,13 +91,14 @@ async def main():
     url = f"https://www.google.com/maps/search/{query}?{lang}"
 
     async with AsyncWebCrawler(config=browser_config) as crawler:
-        result = await crawler.arun(url, config=crawler_config)
+        result = await crawler.arun(url, config=initial_crawler_config)
 
         if result.success:
-            print(result.markdown)
             print(result.extracted_content)
+            print(result.markdown)
+            # print(result.extracted_content)
 
-            llm_strategy.show_usage()
+            # llm_strategy.show_usage()
         else:
             print(f"Crawl failed: {result.error_message}")
             print(f"Status code: {result.status_code}")
